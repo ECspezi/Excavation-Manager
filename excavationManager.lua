eaeee72592fa487efc2f318b10f5be91b487e3a1
ECspezi <conwecs@gmail.com>
2025-03-01 06:05:14 +0400

    init

---
+++ excavationManager.lua
@@ -0,0 +1,19 @@
+local basalt = require("basalt")
+
+local main = basalt.createFrame()
+
+local modem = peripheral.find("modem", rednet.open)
+if modem == nil then
+    basalt.debug("No modem found")
+end
+
+local hostName = "eMaHost" .. os.getComputerID()
+
+rednet.host("eMaHostPair", hostName)
+
+local notConnected = true
+while notConnected do
+    
+end
+
+basalt.autoUpdate()
