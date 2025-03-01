e8561ea76dffb3506bf2e2e8fe1b683ad489d170
ECspezi <conwecs@gmail.com>
2025-03-01 06:37:05 +0400

    turtle broadcast logic

--- excavationTurtle.lua
+++ excavationTurtle.lua
@@ -9,11 +9,55 @@ end
 
 local hostName = "eMaTurtle" .. os.getComputerID()
 
-rednet.host("eMaHostPair", hostName)
+local pairLabel = main:addLabel()
+    pairLabel:setText("Pair this turtle")
+    pairLabel:setPosition(10, 2)
+    pairLabel:setFontSize(2)
 
-local notConnected = true
-while notConnected do
+local pairText = main:addLabel()
+    pairText:setText("Click to start pairing to host")
+    pairText:setPosition(5, 5)
+
+local stopPairButton = main:addButton()
+    stopPairButton:hide()
+    stopPairButton:setPosition(14, 7)
+    stopPairButton:setText("Stop")
+    stopPairButton:setBackground(colors.red)
+    stopPairButton:setForeground(colors.white)
+    stopPairButton:setSize(13, 3)
+    stopPairButton:onClick(function() 
+        stopPairButton:setBackground(colors.white)
+    end)
+    stopPairButton:onRelease(function() 
+        stopPairButton:setBackground(colors.red)
+        stopPairing()
+    end)
+
+local pairButton = main:addButton()
+    pairButton:setPosition(14, 7)
+    pairButton:setText("Broadcast")
+    pairButton:setBackground(colors.green)
+    pairButton:setForeground(colors.white)
+    pairButton:setSize(13, 3)
+    pairButton:onClick(function() 
+        pairButton:setBackground(colors.white)
+    end)
+    pairButton:onRelease(function() 
+        pairButton:setBackground(colors.green)
+        pairWithHost()
+    end)
+
+function stopPairing()
+    rednet.unhost("eMaHostPair")
+    stopPairButton:hide()
+    pairButton:show()
     
+end
+
+function pairWithHost()
+    rednet.host("eMaHostPair", hostName)
+    stopPairButton:show()
+    pairButton:hide()
 end
 
 basalt.autoUpdate()
